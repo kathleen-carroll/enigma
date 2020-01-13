831f6575e5de310600bfd5f60c775f64a01de951
Kathleen Carroll <khakicarroll@comcast.net>
2020-01-13 11:55:19 -0700

    Create shift letters method and pass tests

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -10,9 +10,27 @@ class ShiftTest < Minitest::Test
   def setup
     @offset1 = Offset.new
     @key1 = Key.new
+    @key1.value = 23431
     @shift = Shift.new(@key1, @offset1)
     @message = "Kathleen"
+    @message2 = "Kathlee!"
+    @message3 = "K t!lee_"
     @encrypt = Encrypt.new(@message, @key1, @offset1)
+    @encrypt2 = Encrypt.new(@message2, @key1, @offset1)
+    @encrypt3 = Encrypt.new(@message3, @key1, @offset1)
+    #a = 23 + 4 = 27
+    #b = 34 + 4 = 38
+    #c = 43 + 0 = 43
+    #d = 31 + 0 = 31
+
+    #k = k
+    #a = l
+    #t = i
+    #h = l
+    #l = l
+    #e = p
+    #e = u
+    #n = r
   end
 
   def test_it_exists
@@ -25,5 +43,11 @@ class ShiftTest < Minitest::Test
              "c" => ["t", "e"],
              "d" => ["h", "n"]}
     assert_equal expected, @encrypt.message_breakout
+  end
+
+  def test_it_can_shift_the_letters
+    assert_equal "klillpur", @encrypt.shift_letters(@shift)
+    assert_equal "klillpu!", @encrypt2.shift_letters(@shift)
+    assert_equal "kki!lpu_", @encrypt3.shift_letters(@shift)
   end
 end
