9c0bfff4c10165f58fdf5636570db3e280424c76
Kathleen Carroll <khakicarroll@comcast.net>
2020-01-14 11:59:16 -0700

    Update decrypt method to properly shift back when the shift needs to start again from 27. Still need to test more

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -24,27 +24,41 @@ class Decrypt < Encrypt
     message_breakout.each do |shift_value, letters|
       letters.each do |letter|
         if shift_value == "a" && shift.character_position.key(letter.downcase) != nil
-          lookup_value = shift.character_position.key(letter.downcase).to_i - shift.a%27
-          lookup_value -= 27 if lookup_value > 27
+          current_position = shift.character_position.key(letter.downcase).to_i
+          lookup_value = current_position - shift.a%27
+          lookup_value = current_position + (27 - shift.a%27) if shift.a%27 > current_position
+          lookup_value = 27 if shift.a%27 == current_position
+          # lookup_value -= 27 if lookup_value > 27
           a << shift.character_position[lookup_value.to_s]
         elsif shift_value == "a" && shift.character_position.key(letter.downcase) == nil
           a << letter
         elsif shift_value == "b" && shift.character_position.key(letter.downcase) != nil
-          lookup_value = shift.character_position.key(letter.downcase).to_i - shift.b%27
-          lookup_value -= 27 if lookup_value > 27
+          current_position = shift.character_position.key(letter.downcase).to_i
+          lookup_value = current_position - shift.b%27
+          lookup_value = current_position + (27 - shift.b%27) if shift.b%27 > current_position
+          lookup_value = 27 if shift.b%27 == current_position
+          # lookup_value = shift.character_position.key(letter.downcase).to_i - shift.b%27
+          # lookup_value -= 27 if lookup_value > 27
           b << shift.character_position[lookup_value.to_s]
         elsif shift_value == "b" && shift.character_position.key(letter.downcase) == nil
           b << letter
         elsif shift_value == "c" && shift.character_position.key(letter.downcase) != nil
-          require "pry"; binding.pry
-          lookup_value = shift.character_position.key(letter.downcase).to_i - shift.c%27
-          lookup_value -= 27 if lookup_value > 27
+          current_position = shift.character_position.key(letter.downcase).to_i
+          lookup_value = current_position - shift.c%27
+          lookup_value = current_position + (27 - shift.c%27) if shift.c%27 > current_position
+          lookup_value = 27 if shift.c%27 == current_position
+          # lookup_value = ((lookup_value * -1) + 1) + current_position if lookup_value > 27
+          # lookup_value = 1 + (-lookup_value + shift.character_position.key(letter.downcase).to_i) if lookup_value/-lookup_value == -1
           c << shift.character_position[lookup_value.to_s]
         elsif shift_value == "c" && shift.character_position.key(letter.downcase) == nil
           c << letter
         elsif shift_value == "d" && shift.character_position.key(letter.downcase) != nil
-          lookup_value = shift.character_position.key(letter.downcase).to_i - shift.d%27
-          lookup_value -= 27 if lookup_value > 27
+          # lookup_value = shift.character_position.key(letter.downcase).to_i - shift.d%27
+          # lookup_value -= 27 if lookup_value > 27
+          current_position = shift.character_position.key(letter.downcase).to_i
+          lookup_value = current_position - shift.d%27
+          lookup_value = current_position + (27 - shift.d%27) if shift.d%27 > current_position
+          lookup_value = 27 if shift.d%27 == current_position
           d << shift.character_position[lookup_value.to_s]
         elsif shift_value == "d" && shift.character_position.key(letter.downcase) == nil
         d << letter
--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -15,9 +15,15 @@ class DecryptTest < Minitest::Test
     @shift = Shift.new(@key1, @offset1)
     @message = "Kathleen"
     @decrypt_message = "pjlrqnxx"
+    @decrypt_message2 = "klillpur"
+    @decrypt_message3 = "klillpu!"
+    @decrypt_message4 = "kki!lpu_"
     @encrypt = Encrypt.new(@message, value, offset)
+    @encrypt2 = Encrypt.new(@message, value)
     @decrypt = Decrypt.new(@decrypt_message, value, offset)
-    @decrypt2 = Decrypt.new(@decrypt_message, value)
+    @decrypt2 = Decrypt.new(@decrypt_message2, value)
+    @decrypt3 = Decrypt.new(@decrypt_message3, value)
+    @decrypt4 = Decrypt.new(@decrypt_message4, value)
   end
 
   def test_it_exists
@@ -28,12 +34,15 @@ class DecryptTest < Minitest::Test
     assert_equal "23431", @decrypt.key.value
     assert_equal "250894", @decrypt.offset.date
     assert_equal "23431", @decrypt2.key.value
-    assert_equal "130120", @decrypt2.offset.date
+    assert_equal "140120", @decrypt2.offset.date
   end
 
   def test_it_can_breakdown_the_message
     # expected
     # assert_equal expected,
-    @decrypt.decode
+    assert_equal "kathleen", @decrypt.decode
+    assert_equal "kathleen", @decrypt2.decode
+    assert_equal "kathlee!", @decrypt3.decode
+    assert_equal "k t!lee_", @decrypt4.decode
   end
 end
