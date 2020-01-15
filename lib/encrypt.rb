0d415e67dc1797672c1b954c0ed71f362a7fa4e8
Kathleen Carroll <khakicarroll@comcast.net>
2020-01-14 19:59:31 -0700

    Update encrypt class to enhance testing.  Add stubs to test for randomness

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,7 @@
 require_relative "shift"
 
 class Encrypt
-  attr_reader :key, :offset, :message
+  attr_reader :key, :offset, :message, :a, :b, :c, :d
 
   def initialize(message, key_value = key_gen, offset_value = offset_gen)#key_value = key_gen, offset_value = offset_gen)
     @message = message
@@ -89,45 +89,48 @@ class Encrypt
     # offset
     shift = Shift.new(@key, @offset)
     # encoded = {}
-    a = []
-    b = []
-    c = []
-    d = []
+    @a = []
+    @b = []
+    @c = []
+    @d = []
 
     message_breakout.each do |shift_value, letters|
       letters.each do |letter|
         if shift_value == "a" && shift.character_position.key(letter.downcase) != nil
           lookup_value = shift.character_position.key(letter.downcase).to_i + shift.a%27
           lookup_value -= 27 if lookup_value > 27
-          a << shift.character_position[lookup_value.to_s]
+          @a << shift.character_position[lookup_value.to_s]
         elsif shift_value == "a" && shift.character_position.key(letter.downcase) == nil
-          a << letter
+          @a << letter
         elsif shift_value == "b" && shift.character_position.key(letter.downcase) != nil
           lookup_value = shift.character_position.key(letter.downcase).to_i + shift.b%27
           lookup_value -= 27 if lookup_value > 27
-          b << shift.character_position[lookup_value.to_s]
+          @b << shift.character_position[lookup_value.to_s]
         elsif shift_value == "b" && shift.character_position.key(letter.downcase) == nil
-          b << letter
+          @b << letter
         elsif shift_value == "c" && shift.character_position.key(letter.downcase) != nil
           lookup_value = shift.character_position.key(letter.downcase).to_i + shift.c%27
           lookup_value -= 27 if lookup_value > 27
-          c << shift.character_position[lookup_value.to_s]
+          @c << shift.character_position[lookup_value.to_s]
         elsif shift_value == "c" && shift.character_position.key(letter.downcase) == nil
-          c << letter
+          @c << letter
         elsif shift_value == "d" && shift.character_position.key(letter.downcase) != nil
           lookup_value = shift.character_position.key(letter.downcase).to_i + shift.d%27
           lookup_value -= 27 if lookup_value > 27
-          d << shift.character_position[lookup_value.to_s]
+          @d << shift.character_position[lookup_value.to_s]
         elsif shift_value == "d" && shift.character_position.key(letter.downcase) == nil
-        d << letter
+        @d << letter
         end
       end
     end
+  end
 
-    encoded = {:a => a,
-               :b => b,
-               :c => c,
-               :d => d}
+  def encode
+    shift_letters
+    encoded = {:a => @a,
+               :b => @b,
+               :c => @c,
+               :d => @d}
 
     encoded
 
